f01da13d3c45ea19a978ce28608cee2784bc84fd
wakasa18 <gmayla0918@gmail.com>
2026-08-19 09:56:49 +0800

    Fixed white dropdown text

--- app/Views/partials/theme_head.php
+++ app/Views/partials/theme_head.php
@@ -3,6 +3,6 @@
 <meta name="theme-color" content="#050816">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="stylesheet" href="<?= base_url('assets/css/theme-base.v3.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/css/theme-base.v4.css') ?>">
 <link id="retroFontStyles" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600;700&family=Rajdhani:wght@500;600;700&display=swap" media="print">
 <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Orbitron:wght@600;700&family=Rajdhani:wght@500;600;700&display=swap"></noscript>
